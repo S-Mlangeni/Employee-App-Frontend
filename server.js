2ad2c26057e5491ed793669c549fef4def235236
S-Mlangeni <smlangeni21@gmail.com>
2022-08-09 22:09:25 +0200

    update

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + "/dist/app-frontend/browser"));
+app.use(express.static(__dirname + "/dist/app-frontend"));
 
 app.listen(process.env.PORT || 8000, () => {
     console.log("Server is running");
 });
-//To allow angular to handling routing instead of this server file:
+
 app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname + "/dist/app-frontend/browser/index.html"));
+    res.sendFile(path.join(__dirname + "/dist/app-frontend/index.html"));
 })
